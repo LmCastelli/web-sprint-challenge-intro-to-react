7a84bdb15f940f0bdb913d8555aeeedbe1a4d2af
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-08 12:31:30 -0500

    basic framework complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Character from './components/Character'
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [characters, setChar] = useState([]);
+  const [featuredCharacter, setFeatured] = useState('1')
+
+
+  const pickCharacter = idx => {
+    setFeatured(idx)
+  }
+
+  const closeCharacter = () => {
+    setFeatured(null)
+  }
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +22,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+      .then(res => {
+        setChar(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+    {
+      characters.map(char => {
+        return <Character key={char.name} action={pickCharacter} info={char} />
+      })
+    }
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 2px solid red;
+    font-size: larger;
+
+`
+const Button = styled.button`
+    font-size: larger;
+
+
+`
+export default function Character({info, action}) {
+    return (
+        <StyledCharacter>
+            {info.name}
+            <Button onClick={() => action(info)}>
+                Details
+            </Button>
+        </StyledCharacter>
+    )
+}
