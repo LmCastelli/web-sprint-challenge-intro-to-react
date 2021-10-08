f876ffa20b6a6ee1082b265c6505fadd9739e10b
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-10-08 15:43:28 -0500

    Started making image appear of character, gym break

--- src/components/Details.js
+++ src/components/Details.js
@@ -19,6 +19,14 @@ export default function Details(props) {
         .catch(err => {console.error(err)})
     }, [characterIndex])
 
+    const srcObj = {
+    "Luke Skywalker" : "https://images2.minutemediacdn.com/image/fetch/w_850,h_560,c_fill,g_auto,f_auto/https%3A%2F%2Fdorksideoftheforce.com%2Ffiles%2F2021%2F01%2FLuke-Skywalker-Return-of-the-Jedi-850x560.jpg",
+    "C-3PO" : "https://upload.wikimedia.org/wikipedia/en/5/5c/C-3PO_droid.png",
+    "R2-D2" : "https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/07/1487160686-r2-d2.jpg?resize=768:*",
+    "Darth Vader" : "https://i.guim.co.uk/img/media/0318cf958ab0d6dbfc1d8a52c181532c9e81481a/87_0_1618_971/master/1618.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=6018293cd294f3b2e24b16e0badaf17f",
+    "Leia Organa" : "https://static.wikia.nocookie.net/fictupedia/images/1/1d/Leia-princess-leia-organa-solo-skywalker-9301321-576-1010.jpg/revision/latest/scale-to-width-down/180?cb=20170120065805",
+    "Owen Lars" : "https://static.wikia.nocookie.net/starwars/images/9/91/OwenLarsHS-SWE.jpg/revision/latest/scale-to-width-down/700?cb=20120428164235"
+    }
     return (
         <StyledDetails>
             <h2>Character Details:</h2>
@@ -26,7 +34,10 @@ export default function Details(props) {
                 details !== null && 
                 
                 <>
+                    <h2>{details.name}</h2>
                     <p>{details.name} is {details.mass} kilograms and {details.height} centimeters tall </p>
+                    <p>Film appearances: {details.films}</p>
+                    
                 </>
             }
             <button onClick={close}>Close</button>
